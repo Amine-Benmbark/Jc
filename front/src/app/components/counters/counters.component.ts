26376c1b64533b46d00c8ba676a3442daa8b9316
jc rangon <jc.rangon@gmail.com>
2023-10-08 04:58:41 +0200

    dynamic carousel

--- front/src/app/components/counters/counters.component.ts
+++ front/src/app/components/counters/counters.component.ts
@@ -1,10 +1,60 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ArticleService } from 'src/app/services/article.service';
+import { OwlOptions } from 'ngx-owl-carousel-o';
+
 
 @Component({
   selector: 'app-counters',
   templateUrl: './counters.component.html',
   styleUrls: ['./counters.component.css']
 })
-export class CountersComponent {
+export class CountersComponent implements OnInit {
+  articles!: any[]
+  loaded!: boolean
+  requestStarted!: boolean
+  customOptions: OwlOptions = {
+    loop: false,
+    mouseDrag: true,
+    touchDrag: true,
+    pullDrag: true,
+    dots: true,
+    navSpeed: 700,
+    navText: ['', ''],
+    responsive: {
+      0: {
+        items: 1
+      },
+      768: {
+        items: 2
+      },
+      992: {
+        items: 2
+      },
+      1200: {
+        items: 3
+      }
+    },
+    nav: true
+  }
+
+  constructor(
+    private articleService: ArticleService
+  ) {
+    this.loaded = false
+    this.requestStarted = false;
+  }
+
+  ngOnInit(): void {
+    this.loaded = false
+    this.requestStarted = true
+    this.articleService.getArticlesAll().subscribe({
+      next: (res: any) => {
+        console.log(res)
+        this.articles = res.data
+        this.loaded = true
+        this.requestStarted = false;
+      }
+    })
+  }
 
 }
